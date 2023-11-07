af3bf98a7d80a7063121709d2be7329d83badeb5
cocofox1902 <colas.renard1902@gmail.com>
2023-11-07 14:25:47 +0000

    add all

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from "react";
-import menuData from '../menu.json';
+import menuData from "../menu.json";
 
 function Navbar() {
   const categories = Object.keys(menuData);
   const [activeCategory, setActiveCategory] = useState(categories[0]);
 
   useEffect(() => {
+    const handleScroll = () => {
+      const sections = categories.map((category) =>
+        document.getElementById(category)
+      );
+      const sectionInView = sections.find((section) => {
+        const rect = section.getBoundingClientRect();
+        return (
+          rect.top <= window.innerHeight / 2 &&
+          rect.bottom > window.innerHeight / 2
+        );
+      });
+
+      if (sectionInView) {
+        setActiveCategory(sectionInView.id);
+      }
+    };
+
     window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
-
-  const handleScroll = () => {
-    const sections = categories.map((category) => document.getElementById(category));
-    const sectionInView = sections.find((section) => {
-      const rect = section.getBoundingClientRect();
-      return rect.top <= window.innerHeight / 2 && rect.bottom > window.innerHeight / 2;
-    });
-
-    if (sectionInView) {
-      setActiveCategory(sectionInView.id);
-    }
-  };
+  }, [categories]);
 
   return (
     <nav className="bg-white p-4 text-center top-0 z-50 sticky">
@@ -33,7 +38,7 @@ function Navbar() {
             <a
               href={`#${category}`}
               className={`transition-all duration-200 p-1 text-[3rem] ${
-                activeCategory === category ? 'opacity-100' : 'opacity-50'
+                activeCategory === category ? "opacity-100" : "opacity-50"
               }`}
             >
               {category}
